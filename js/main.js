19e5426979730de70e40c72c245b949ec39236d0
alvarezcodes <alvarezcodes@gmail.com>
2022-08-15 15:51:00 -0400

    2nd commit. Updated mobile response and mobile menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('LINKED');
+// console.log('LINKED');
+
+// TARGET THE HAMBURGER MENU BUTTON
+const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay-show');
+const menu = document.getElementById('mobile-menu');
+
+btn.addEventListener('click', navToggle);
+
+
+function navToggle(){
+    btn.classList.toggle('open');
+    overlay.classList.toggle('overlay-show-menu');
+
+    // prevent page scroll when we toggle ON the menu
+    document.body.classList.toggle('stop-scrolling');
+
+    // mobiel menu
+    menu.classList.toggle('show-menu');
+}
